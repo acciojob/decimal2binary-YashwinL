a3ccec8ff9fdf2de36fdb10ad33f1b5d94ca5c87
YashwinL <54934481+YashwinL@users.noreply.github.com>
2023-05-03 20:50:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-function threeSum(arr, target) {
-// write your code here
-console.log(target);
-  console.log("hi")
+function binary(){
+	let val = document.getElementById("deicmalNumber").value;
+
+	let binarynumber="";
+
+	while(val>0){
+		var reminder = val%2;
+		binarynumber = remainder + binarynumber;
+		val = Math.Floor(val/2);
+	}
+
+	document.getElementById("binaryNumber").innerHTML = binarynumber;
 }
 
 
